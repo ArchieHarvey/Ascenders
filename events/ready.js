c74a8f73002d9f0224dd3eca2a73393732c56065
ArchieHarvey <64043239+ArchieHarvey@users.noreply.github.com>
2021-02-24 12:25:17 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,23 +1,38 @@
 const client = require('../ascenders')
 const config = require('../config.json');
 const prefix = config.prefix
-
+const { MessageEmbed } = require('discord.js')
 client.on('ready', () => {
     console.log(`${client.user.username} ✅ has logged on`);
 
-    const arrayofstatus = [
-        //`${client.guilds.cache.size} servers`,
-        //`${client.channels.cache.size} channels`,
-        `with ${client.guilds.cache.reduce((c, g) => c + g.memberCount, 0)} users`,
-        //`${client.user.tag} discord bot!`,
-        //`run !help`
-    ];
-
-    let index = 0;
+     let index = 0;
     setInterval(() => {
-        if(index === arrayofstatus.length) index = 0;
+        //client.user.setActivity(`Ascender Music`, { type: 'PLAYING' })
+        //client.user.setActivity(`${client.guilds.cache.size} Servers`, { type: 'COMPETING' })
+       // client.user.setActivity(`with ${client.guilds.cache.reduce((c, g) => c + g.memberCount, 0)} users`, { type: 'PLAYING' })
+        //client.user.setActivity(`RAM: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}MB`, { type: 'PLAYING' })
+       // client.user.setActivity(`since ${Math.floor(process.uptime() / 60)} minutes`, { type: 'PLAYING' })
+        //client.user.setActivity(`Heap Total: ${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} MB`, { type: 'PLAYING' })
+        const arrayofstatus = [
+            `with ${client.guilds.cache.reduce((c, g) => c + g.memberCount, 0)} users`,
+            `since ${Math.floor(process.uptime() / 60)} minutes`,
+            `in ${client.guilds.cache.size} Servers`,
+            `Ascender Music`,
+            `Ascender Radio`,
+            `with ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}MB RAM`
+            ]
+            
+            if (index === arrayofstatus.length) index = 0;
         const status = arrayofstatus[index];
-        client.user.setActivity(status);
+        client.user.setActivity(status, {type: "PLAYING"});
         index++;
-    }, 5000)
+    }, 10000)
+
+
+    const upchannel = client.channels.cache.get('810530165331066942')
+    const upembed = new MessageEmbed()
+        .setThumbnail(client.user.avatarURL())
+        .setTitle("Bot Online")
+        .setDescription("Hey I am back online")
+    upchannel.send(upembed)
 });
