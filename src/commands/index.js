f454c536a165e591b17d9a8021822adbdcb40b82
Archie <64043239+ArchieHarvey@users.noreply.github.com>
2026-02-05 10:50:51 +0530

    Add interactive avatar command with timed requester-only buttons

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,6 +1,7 @@
 import { pingCommand } from "./ping.js";
 import { updateCommand } from "./update.js";
+import { avatarCommand } from "./avatar.js";
 
-export const commands = [pingCommand, updateCommand];
+export const commands = [pingCommand, updateCommand, avatarCommand];
 
 export const commandMap = new Map(commands.map((command) => [command.data.name, command]));
