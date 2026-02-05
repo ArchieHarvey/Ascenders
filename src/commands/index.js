b2fd1a9e7220be991571643448fa0c44003ccddc
Archie <64043239+ArchieHarvey@users.noreply.github.com>
2026-02-05 12:15:04 +0530

    chore: bump package version and auto-install git hooks

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,7 +1,8 @@
 import { pingCommand } from "./ping.js";
 import { updateCommand } from "./update.js";
 import { avatarCommand } from "./avatar.js";
+import { restartCommand } from "./restart.js";
 
-export const commands = [pingCommand, updateCommand, avatarCommand];
+export const commands = [pingCommand, updateCommand, avatarCommand, restartCommand];
 
 export const commandMap = new Map(commands.map((command) => [command.data.name, command]));
